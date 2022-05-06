d0c404e572ee7362cd8912a615360d8516492a4d
ShonaliRomeshika <shonaliromeshika05@gmail.com>
2022-05-06 20:59:09 +0530

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import App from "./App"
+import Header from "./components/Header";
 
 
 function App() {
     return (
       <React.Fragment>
         <header>
-          
+           <Header />
         </header>
         </React.Fragment>
 
