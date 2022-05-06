e76a3dcd89c4a82831389c2095c042cb8b9f0928
ShonaliRomeshika <shonaliromeshika05@gmail.com>
2022-05-06 21:40:33 +0530

    update Item.js

--- frontend/src/components/Item/Item.js
+++ frontend/src/components/Item/Item.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import { Button } from "@mui/material";
-import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+//import axios from "axios";
+import { useNavigate } from "react-router-dom";
 //import "./Book.css";
 
 const Item = (props) => {
     const history = useNavigate();
     const { id, iname, price, image } = props.item;
-    const deleteHandler = async () => {
-      await axios
-        .delete(`http://localhost:5000/items/id`)
-        .then((res) => res.data)
-        .then(() => history("/"))
-        .then(() => history("/items"));
-    };
+    
   return (
     <div className="card">
       <img src={image} alt={iname} />
