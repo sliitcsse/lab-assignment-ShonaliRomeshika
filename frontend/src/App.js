a29e386b1b892b98d40f3d62a750eb9addc1ada4
ShonaliRomeshika <shonaliromeshika05@gmail.com>
2022-05-06 20:30:47 +0530

    create app.js

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function App() {
+    return (
+      <React.Fragment>
+        <header>
+          
+        </header>
+        </React.Fragment>
+
+    );
+  }
+
+
+  export default App;
