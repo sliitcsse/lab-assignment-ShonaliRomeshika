aae75483c3721d379a86a923c79667eb9033cebe
ShonaliRomeshika <shonaliromeshika05@gmail.com>
2022-05-06 22:25:02 +0530

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,12 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import AddItem from "./components/Item/AddItem";
 import Items from "./components/Item/Items";
+import CartItems from "./components/Cart/CartItems";
+import AddCart from "./components/Cart/AddCart";
+import AddCustomer from "./components/Customer/AddCustomer";
+import Customers from "./components/Customer/Customers";
+import AddPromotion from "./components/Promotion/Promotions";
+import Promotions from "./components/Promotion/Promotions";
 
 
 function App() {
@@ -18,6 +24,12 @@ function App() {
           <Route path="/" element={<Home />} exact />
           <Route path="/addItem" element={<AddItem />} exact />
           <Route path="/items" element={<Items />} exact />
+          <Route path="/cartItems" element={<CartItems />} exact />
+          <Route path="/addCart" element={<AddCart />} exact />
+          <Route path="/addCustomer" element={<AddCustomer />} exact />
+          <Route path="/customers" element={<Customers />} exact />
+          <Route path="/addPromotion" element={<AddPromotion />} exact />
+          <Route path="/promotions" element={<Promotions />} exact />
         </Routes>
         </main>
         </React.Fragment>
